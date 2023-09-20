6d63f6f6a5a067f3d6a1be02f5b1e6d5b334708e
Nikita Panwar <78353504+nikita22panwar@users.noreply.github.com>
2023-09-20 11:10:29 +0530

    Add PWA functionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// Check if the browser supports service workers
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        console.log("Service Worker loaded");
+        navigator.serviceWorker
+            .register("/service-worker.js") // Path to your service worker file
+            .then((registration) => {
+                console.log(
+                    "Service Worker registered with scope:",
+                    registration.scope
+                );
+            })
+            .catch((error) => {
+                console.error("Service Worker registration failed:", error);
+            });
+    });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
